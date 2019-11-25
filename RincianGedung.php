75654358a665b6e83d26c5fd3cd15812c3103e75
Dtake88 <56423564+Dtake88@users.noreply.github.com>
2019-11-26 01:27:16 +0700

    history gedung

--- RincianGedung.php
+++ RincianGedung.php
@@ -17,6 +17,15 @@ echo "<div class='row'>
     inovasi, dan nilai-nilai keindonesiaan.
     
     </div>
+    <br>
+        <div>
+        <h4>Misi</h4><hr>
+        <ul>
+        <li>Memberikan materi pendidikan dan pengajaran terbaik di bidang sains, teknologi, dan desain, dengan beberapa warna keindonesiaan di dalamnya, mengacu pada berbagai standar internasional, disesuaikan dengan situasi dan kondisi kebutuhan dan perkembangan sosial, ekonomi, dan budaya masyarakat Asia Tenggara.</li>
+        <li>Melakukan penelitian berstandar internasional di bidang sains, teknologi, dan desain, sesuai kebutuhan dan perkembangan sosial, ekonomi, dan budaya masyarakat Asia Tenggara, menggunakan keindonesiaan sebagai bagian dari elemen penelitian.</li>
+        <li>Menyediakan berbagai solusi inovatif dan unggulan berbasis sains, teknologi, dan desain bagi berbagai masalah sosial, ekonomi, dan budaya, mengacu pada berbagai standar internasional, khususnya dalam menghadapi pasar masyarakat ekonomi ASEAN (AEC)</li>
+        </ul>
+        </div>
 </div>
 <div class='col-md-9'>
 <div class='container my-5 py-5 z-depth-1'>
@@ -33,7 +42,7 @@ echo "<div class='row'>
 
       <!--Image-->
       <div class='view overlay z-depth-1-half'>
-        <img src='assets/img/GedungE.jpg' class='img-fluid' alt=''>
+        <img src='assets/img/gedungE/gedungE.jpg' class='img-fluid' alt=''>
         <a href=''>
           <div class='mask rgba-white-light' id='btnGambarRincian'></div>
         </a>
@@ -54,13 +63,193 @@ echo "<div class='row'>
 
   </div>
   <!--Grid row-->
+  
+  <br>  
+  
+  <!--Grid row-->
+  <div class='row'>
 
+    <!--Grid column-->
+    <div class='col-md-3 mb-2 mb-md-0'>
+    <div class='view overlay z-depth-1-half'>
+      <!--Image-->
+        <img src='assets/img/gedungE/elib.jpg' class='img-fluid' alt=''>
+        <a href=''>
+          <div class='mask rgba-white-light' id='btnGambarRincian'></div>
+        </a>
+    </div>
+    </div>
+    <!--Grid column-->
 
+    <!--Grid column-->
+    <div class='col-md-9 mb-4 mb-md-0'>
+
+      <h3 class='font-weight-bold'>E-Library</h3>
+
+      <p class='text-muted'>
+      Komputer canggih yang dilengkapi dengan internet berkecepatan tinggi menjadi daya tarik tersendiri bagi e-library. Kenyamanan tersebut dimanfaatkan mahasiswa untuk membantu mendapatkan segala resource yang dibutuhkan untuk menyelesaikan tugas maupun proyek perkuliahan.
+      </p>
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!--Grid row-->
+  
+  <br>  
+  
+  <!--Grid row-->
+  <div class='row'>
+
+    <!--Grid column-->
+    <div class='col-md-3 mb-2 mb-md-0'>
+    <div class='view overlay z-depth-1-half'>
+      <!--Image-->
+        <img src='assets/img/gedungE/rumKls.jpg' class='img-fluid' alt=''>
+        <a href=''>
+          <div class='mask rgba-white-light' id='btnGambarRincian'></div>
+        </a>
+    </div>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class='col-md-9 mb-4 mb-md-0'>
+
+      <h3 class='font-weight-bold'>Ruang Kelas</h3>
+
+      <p class='text-muted'>
+      Ruang kelas akan selalu menjadi ruang kelas. Namun, berbeda dengan ruang kelas pada umumnya, segala kelas di Tower of The Eagles dilengkapi dengan projector screen siap pakai. Desain interiornya pun menambah semangat belajar.
+      </p>
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!--Grid row-->
+
+  <br>  
+  
+  <!--Grid row-->
+  <div class='row'>
+
+    <!--Grid column-->
+    <div class='col-md-3 mb-2 mb-md-0'>
+    <div class='view overlay z-depth-1-half'>
+      <!--Image-->
+        <img src='assets/img/gedungE/rumPMB.jpg' class='img-fluid' alt=''>
+        <a href=''>
+          <div class='mask rgba-white-light' id='btnGambarRincian'></div>
+        </a>
+    </div>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class='col-md-9 mb-4 mb-md-0'>
+
+      <h3 class='font-weight-bold'>Penerimaan Mahasiswa Baru</h3>
+
+      <p class='text-muted'>
+      Kantor penerimaan, yang paling sering dikunjungi mahasiswa baru. Di kantor ini bekerja beberapa staff dan dosen yang khusus melayani penerimaan mahasiswa baru. Kantor ini juga menjadi gerbang STTS ke komunitas luar.
+      </p>
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!--Grid row-->
+
+  <br>  
+  
+  <!--Grid row-->
+  <div class='row'>
+
+    <!--Grid column-->
+    <div class='col-md-3 mb-2 mb-md-0'>
+    <div class='view overlay z-depth-1-half'>
+      <!--Image-->
+        <img src='assets/img/gedungE/rumTeater.jpg' class='img-fluid' alt=''>
+        <a href=''>
+          <div class='mask rgba-white-light' id='btnGambarRincian'></div>
+        </a>
+    </div>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class='col-md-9 mb-4 mb-md-0'>
+
+      <h3 class='font-weight-bold'>Ruang Teater</h3>
+
+      <p class='text-muted'>
+      Jenis ruang kelas yang paling banyak dimininati mahasiswa maupun dosen pengajar. Ruangan ini menciptakan suasana akademik yang cukup baik sehingga meningkatkan minat belajar. Terdapat total 3 ruang kelas berjenis ini.                  
+      </p>
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!--Grid row-->
+  
+  <br>  
+  
+  <!--Grid row-->
+  <div class='row'>
+
+    <!--Grid column-->
+    <div class='col-md-3 mb-2 mb-md-0'>
+    <div class='view overlay z-depth-1-half'>
+      <!--Image-->
+        <img src='assets/img/gedungE/rumBAU.jpg' class='img-fluid' alt=''>
+        <a href=''>
+          <div class='mask rgba-white-light' id='btnGambarRincian'></div>
+        </a>
+    </div>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class='col-md-9 mb-4 mb-md-0'>
+
+      <h3 class='font-weight-bold'>Biro Administrasi Keuangan</h3>
+
+      <p class='text-muted'>
+      Biro yang dibawahi oleh Pembantu Ketua II ini menangani segala bentuk kegiatan finansial STTS, mulai dari penerimaan pembayaran SPP, hingga operasional lingkup kerja kampus. Sepi merupakan kata yang paling tidak cocok dengan ruangan ini.
+      </p>
+
+    </div>
+    <!--Grid column-->
+
+  </div>
+  <!--Grid row-->
 </section>
 <!--Section: Content-->
 
 
+
 </div>
+<nav>
+  <ul class='pagination pg-purple'>
+    <li class='page-item'>
+      <a class='page-link' aria-label='Previous'>
+        <span aria-hidden='true'>&laquo;</span>
+        <span class='sr-only'>Previous</span>
+      </a>
+    </li>
+    <li class='page-item active'><a class='page-link'>1</a></li>
+    <li class='page-item'><a class='page-link'>2</a></li>
+    <li class='page-item'><a class='page-link'>3</a></li>
+    <li class='page-item'><a class='page-link'>4</a></li>
+    <li class='page-item'><a class='page-link'>5</a></li>
+    <li class='page-item'>
+      <a class='page-link' aria-label='Next'>
+        <span aria-hidden='true'>&raquo;</span>
+        <span class='sr-only'>Next</span>
+      </a>
+    </li>
+  </ul>
+</nav>
 </div>
 </div>";
 ?>
